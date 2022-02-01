2a141b38349fd4d3626dabfa16d8614ca5621acd
Rishi Raj <rraj@pr.business>
2022-02-01 17:46:33 -0500

    Modified the events.php for project logic

---
+++ ReadMe.md
@@ -0,0 +1,26 @@
+# Fooda Coding Challenge 
+
+
+## Installtion 
+(**Debian**)
+1. Please clone this repository in the desired folder. 
+2. To view the code open with any IDE.  
+3. Open terminal in the project directory. To run please make sure `PHP 7.8^` is installed on the system.
+4. Type `php events.php`
+5. The result report comes out as a json output in the same folder `results.json`
+
+## Dependencies 
+1. PHP -v 7.8^ 
+    Installtion Instructions for Debain: https://www.php.net/manual/en/install.unix.debian.php
+2.  Debian Based OS *(Recommended)*
+
+## Sample Output
+The response is a PHP Object from the Event Class functions and gets encoded to json and creates a new file as `results.json`
+```json
+{"1":"Jessica: 14 points with 2 per order."},{"2":"Will: 4 points with 3 per order."}
+```
+
+## Notes
+Thank You again for giving me the oppurtunity to work on the project! Please reach out to me via email `rraj3107@gmail.com` for any questions and concerns regarding this project.
+
+PHP was not the best choice was a project like this but I started it off since I'm currently very hands on with this.
--- events.php
+++ events.php
@@ -28,26 +28,110 @@ class Events{
         return $orders;
     }
 
+    // public function 
     public function getRewardsPoint() {
     
-        $timestampArray = [];
-        $time = [];
+        $timeArray = [];
+        $rewardPointArray = [];
         foreach ($this->data->events as $val) {
             if ($val->action == "new_order"){
-                $timestampArray[] = $val->timestamp;
+                $timeArray = array(getdate((strtotime(str_replace("T"," ",$val->timestamp)))));
+    
+                if ($timeArray[0]["hours"]=="12") {
+                    $reward = round($val->amount/3);
+                    $rewardPointArray[] = ["Name"=> $val->customer,"Rewards" => $reward];
+                } 
+                elseif($timeArray[0]["hours"]=="11" or $timeArray[0]["hours"] =="13") {
+                    $reward = round($val->amount/2);
+                    $rewardPointArray[] = ["Name"=> $val->customer,"Rewards" => $reward];
+                } 
+                elseif ($timeArray[0]["hours"]=="10" or $timeArray[0]["hours"]=="11") {
+                    $reward = round($val->amount/1);
+                    $rewardPointArray[] = ["Name"=> $val->customer,"Rewards" => $reward];
+                } 
+                else {
+                    $rewardPointArray[] = ["Name"=> $val->customer,"Rewards" => "0"];
+                }
             }
         }
-        for($i=0;$i<count($timestampArray);$i++){
-            $temp= explode(" ",str_replace("T"," ",$timestampArray[$i]));
-            $time[]= $temp[1];
-            
+        return $rewardPointArray;
+    }
+
+    function group_by($key, $data) {
+        $result = array();
+    
+        foreach($data as $val) {
+            if(array_key_exists($key, $val)){
+                $result[$val[$key]][] = $val;
+            }else{
+                $result[""][] = $val;
+            }
         }
-        return $time;
+    
+        return $result;
+    }
+
+    //Helper Function to add and sort
+    function addRewards($array){
+        $names = array_column($array, 'Name');
+        array_multisort($names, SORT_ASC, $array);
+    
+        $array_sorted = [];
+        $seen_names = [];
+    
+        foreach($array as $entry){
+            $seen = false;
+            //check if name already processed
+            foreach($seen_names as $name){
+                if($entry['Name'] == $name){
+                    $seen = true;
+                    break;
+                }
+            }
+            if($seen == true){
+                continue;
+            }
+            $cur_entry = $entry;
+            $cur_entry['Rewards'] = 0;
+            $count = 0;
+            foreach($array as $entryTwo){
+                if($entryTwo['Name'] == $cur_entry['Name']){
+                    $cur_entry['Rewards'] += $entryTwo['Rewards'];
+                    $count++;
+                }
+            }
+            $cur_entry["Entries"] = $count;
+            $array_sorted[] = $cur_entry;
+            $seen_names[] = $entry['Name'];
+        }
+        return $array_sorted;
+    }
+
+    public function calculateRewards(){
+        $result = [];
+        $rewardsArray = $this->getRewardsPoint();
+        for ($i=0; $i< count($rewardsArray); $i++){
+            if ($rewardsArray[$i]["Rewards"]< 3 or $rewardsArray[$i]["Rewards"] > 20){
+                $rewardsArray[$i]["Rewards"] = 0;
+            }
+        }
+        $resultToBeFormatted = $this->addRewards($rewardsArray);
+        foreach ($resultToBeFormatted as $key=>&$val){
+            $result[] = [$key+1 =>$val["Name"].": ".$val["Rewards"]." points with ".$val["Entries"]." per order."];
+            var_dump($result);
+        }
+        return $result;
+    }
+
+    // Create the results file 
+    public function printRewardsReport(){
+        $result = $this->calculateRewards();
+        $fp = fopen('results.json',"w");
+        fwrite($fp, json_encode($result));
+        fclose($fp);
     }
 }
 
 $event = new Events; 
-//var_export($event->getNewCustomer());
-//var_export($event->getNewOrder());
-var_export($event->getRewardsPoint());
+$event->printRewardsReport();
 ?>
